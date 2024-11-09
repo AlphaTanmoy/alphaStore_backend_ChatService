557f7dd5c250d66b23a16c69f544ea66fa6de69f
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-10 00:32:33 +0530

    update

---
+++ src/test/kotlin/com/alphaStore/ChatService/ChatServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alphaStore.ChatService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
